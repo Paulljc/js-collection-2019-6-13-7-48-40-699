0d40700a3afb3deec1abc24429c3f46ea672524b
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-10 14:36:15 +0800

    try to complete group count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //implement here
+  const item = {};
+  collection.forEach(value => {
+    if (item[value] == undefined) {
+      item[value] = 1;
+    } else {
+      item[value]++;
+    }
+  })
+  return item;
 }
 
 module.exports = grouping_count;
