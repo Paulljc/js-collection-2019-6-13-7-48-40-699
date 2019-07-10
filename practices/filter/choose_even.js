cf849924ad0943adbea41d66165bee610f85f8a2
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-10 14:31:17 +0800

    complete filter

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_even(collection) {
-
   //implement here
+  const result = [];
+  collection.forEach(element => {
+    if(element % 2 === 0){
+      result.push(element)
+    }
+  });
+  return result;
 }
 
 module.exports = choose_even;
